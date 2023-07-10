1ea3a6545d6dbac5041dcab3b6334ba3166e2a46
SylvioRubens <srjneto@hotmail.com>
2023-07-10 16:55:20 -0300

    Deleting the division feature from our calculator

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -2,6 +2,11 @@ class cal():
     def __init__(self):
         pass
 
+    def checking_consistence(self, x):
+        x = str(x)
+        if not x.replace('.', '', 1).isdigit():
+            raise Exception('The information inputed is not a number. Closing Calculator!')
+
     def add(self, a, b):
         self.checking_consistence(a)
         self.checking_consistence(b)
@@ -23,14 +28,9 @@ class cal():
         b = float(b)
         return a * b
 
-    def divide(self, a, b):
-        self.checking_consistence(a)
-        self.checking_consistence(b)
-        a = float(a)
-        b = float(b)
-        return a / b
-
-    def checking_consistence(self, x):
-        x = str(x)
-        if not x.replace('.', '', 1).isdigit():
-            raise Exception('The information inputed is not a number. Closing Calculator!')
+    # def divide(self, a, b):
+    #     self.checking_consistence(a)
+    #     self.checking_consistence(b)
+    #     a = float(a)
+    #     b = float(b)
+    #     return a / b
